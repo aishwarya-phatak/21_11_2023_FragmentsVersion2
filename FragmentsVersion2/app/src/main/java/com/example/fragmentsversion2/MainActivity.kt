93d66a9d86aab81073710019d6a0173f51227abb
aishwarya <ashp17star@gmail.com>
2023-01-17 17:02:18 +0530

    Fragments, Scrollview

---
+++ FragmentsVersion2/app/src/main/java/com/example/fragmentsversion2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fragmentsversion2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.fragmentsversion2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var counterFragment: CounterFragment
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initFragments()
+        initListeners()
+    }
+
+    private fun initFragments(){
+        counterFragment = supportFragmentManager.findFragmentById(R.id.counterFragment) as CounterFragment
+    }
+    private fun initListeners(){
+        binding.btnReset.setOnClickListener {
+
+        }
+    }
+}
